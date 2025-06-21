c8720cbe4aff3fd080facba529c62f53d342503d
iwaki-toshiyuki <toytoy0714@icloud.com>
2025-06-21 18:15:33 +0900

    Python課題

---
+++ python/Python-TEST/example.py
@@ -0,0 +1,32 @@
+# 課題１
+print("hello world")
+
+# 課題2
+def greet():
+  return "こんにちは"
+
+print(greet())
+
+# 課題3
+def print_name(name):
+  return "私の名前は" + name + "です"
+
+print(print_name("トイトイ"))
+
+#課題4
+def get_greet():
+  return "おはようございます"
+
+print(get_greet())
+
+#課題5
+def add(a,b):
+  return a + b
+
+print(add(6,21))
+
+
+#課題6
+import webbrowser
+url = "file:///Users/iwakitoshiyuki/raretech/python/Python-TEST/example.html"
+webbrowser.open_new_tab(url)
